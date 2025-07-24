c3f2be8625787d054b23d0ff126c85c838443980
ChrisAceXR <chris@acexr.com>
2025-07-23 23:54:18 -0500

    Add dotenv support and update dependencies
    Integrated flutter_dotenv for environment variable management and updated Supabase initialization to use values from .env. Updated several dependencies in pubspec.yaml to newer versions and added .env to .gitignore and assets.

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/core/extensions/hive_extensions.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -9,6 +10,7 @@ import 'package:flutter_application/dependency_injection.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load();
   await _initializeSupabase();
   await _initializeHive();
   configureDependencyInjection();
@@ -20,8 +22,8 @@ void main() async {
 
 Future<void> _initializeSupabase() async {
   await Supabase.initialize(
-    url: "PROJECT_URL",
-    anonKey: "PUBLIC_ANON_KEY",
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
 }
 
@@ -29,3 +31,21 @@ Future<void> _initializeHive() async {
   await Hive.initFlutter();
   await Hive.openThemeModeBox();
 }
+
+/*
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(); // Loads .env
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
+  runApp(MyApp());
+}
+
+*/
